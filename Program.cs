c11f8daa48a5846ae6174fc1d7b4b1457e7e34c5
toandzpro <kimthoai15620@gmail.com>
2024-05-24 04:54:59 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+public class Bàitap9
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        float rate = 23000; // Tỉ giá 23000 VND/USD
+        Console.Write("Nhập số tiền USD: ");
+        float soTienUSD = Convert.ToSingle(Console.ReadLine());
+        float soTienVND = soTienUSD * rate;
+        Console.WriteLine("Giá trị tiền VNĐ là: " + soTienVND);
+    }
+}
